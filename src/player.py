1846a07e49cd0668aae299452fefe7534e549c62
Dai Feng <eduardo@daifengdeMacBook-Pro.local>
2025-01-09 17:26:06 +0900

    an example for a regression test

---
+++ src/player.py
@@ -0,0 +1,13 @@
+def number_conversion(number):
+    dict = {"30": "Stephen",
+            "24": "Kobe",
+            "11": "Ming"}
+    return dict[number]
+
+
+def team_conversion():
+    numbers = ["30", "24", "11"]
+    return ", ".join([number_conversion(number) for number in numbers])
+
+
+print(team_conversion())
--- tests/test_math.py
+++ tests/test_math.py
@@ -1,16 +1,10 @@
-from src import math
+from src import player
 import unittest
 
 
 class TestUtil(unittest.TestCase):
-    def average(self, *args):
-        total = math.sum(*args)
-        return math.div(total, len(args))
-
-    def test_math(self):
-        self.assertEqual(self.average(1, 2, 3), 2)
-        self.assertEqual(self.average(10, 20, 30), 20)
-        self.assertEqual(self.average(1.5, 0, 21), 7.5)
+    def test_team_conversion(self):
+        self.assertEqual(player.team_conversion(), "Stephen, Kobe, Ming")
 
 
 if __name__ == '__main__':
